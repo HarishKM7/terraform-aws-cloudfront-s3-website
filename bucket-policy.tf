5e2512d11cdb24aa4b2c55cf7b5f0c466489d189
Harish KM <harishkm@harishkm.in>
2021-05-05 20:01:04 +0530

    Make bucket public

--- bucket-policy.tf
+++ bucket-policy.tf
@@ -3,8 +3,8 @@ data "aws_iam_policy_document" "bucket_policy" {
     actions   = ["s3:GetObject"]
     resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
     principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
+      type        = "*"
+      identifiers = ["*"]
     }
     condition {
       test     = "StringEquals"
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,15 +1,12 @@
 module "s3_bucket" {
-  source                  = "terraform-aws-modules/s3-bucket/aws"
-  version                 = "2.1.0"
-  attach_policy           = true
-  policy                  = data.aws_iam_policy_document.bucket_policy.json
-  force_destroy           = true
-  attach_public_policy    = true
-  restrict_public_buckets = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  block_public_acls       = true
-  website                 = { index_document = "index.html" }
+  source        = "terraform-aws-modules/s3-bucket/aws"
+  version       = "2.1.0"
+  acl           = "public-read"
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.bucket_policy.json
+  force_destroy = true
+  website       = { index_document = "index.html" }
+
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
